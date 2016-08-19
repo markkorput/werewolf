31934913358b99222acc6f78e2dba035a201d53d
Mark <dr.theman@gmail.com>
2016-08-19 10:48:44 +0000

    refactored test

--- imports/api/tasks.tests.js
+++ imports/api/tasks.tests.js
@@ -22,46 +22,33 @@ if (Meteor.isServer) {
           private: true
         });
       });
-      
-      it('can delete owned task', () => {
-        // Find the internal implementation of the task method so we can
-        // test it in isolation
-        const deleteTask = Meteor.server.method_handlers['tasks.remove'];
- 
-        // Set up a fake method invocation that looks like what the method expects
-        const invocation = { userId };
- 
-        // Verify initial number of tasks
-        assert.equal(Tasks.find().count(), 1);
 
-        // Run the method with `this` set to the fake invocation
-        deleteTask.apply(invocation, [taskId]);
- 
-        // Verify that the method does what we expected
-        assert.equal(Tasks.find().count(), 0);
-      });
-
-      it('cannot delete unowned task', () => {
+      it('can delete task when owned', () => {
         // Find the internal implementation of the task method so we can
         // test it in isolation
         const deleteTask = Meteor.server.method_handlers['tasks.remove'];
  
         // Set up a fake method invocation that looks like what the method expects
         const wrongId = userId + '_wrong';
-        const invocation2 = { wrongId };
+        const wrong_invocation = { wrongId };
  
         // Verify initial number of tasks
         assert.equal(Tasks.find().count(), 1);
 
-        try {
-            // Run the method with `this` set to the fake invocation
-            deleteTask.apply(invocation2, [taskId]);
-        } catch (err) {
-            assert.equal(err.error, "not-authorized");
-        }
+        // Verify that wrong invocation fails an throws error
+        assert.throws(() => deleteTask.apply(wrong_invocation, [taskId]), Meteor.Error, 'not-authorized');
 
         // Verify that no tasks were deleted
         assert.equal(Tasks.find().count(), 1);
+
+        // Setup invocation that pretends to be logged in as owning user
+        const invocation = { userId };
+       
+        // Run the method with `this` set to the fake invocation
+        assert.doesNotThrow(() => deleteTask.apply(invocation, [taskId]));
+ 
+        // Verify that the method does what we expected
+        assert.equal(Tasks.find().count(), 0);
       });
     });
   });
