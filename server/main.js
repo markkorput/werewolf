338b337942b0f81939acbc880ee6057a17fa6c44
Mark <dr.theman@gmail.com>
2016-08-19 12:06:08 +0000

    auto-login to guest account

--- server/main.js
+++ server/main.js
@@ -1,6 +1,13 @@
 import { Meteor } from 'meteor/meteor';
 import '../imports/api/tasks.js';
+import { Accounts } from 'meteor/accounts-base';
+import { AccountsGuest } from 'meteor/artwells:accounts-guest';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  Accounts.removeOldGuests()
+  AccountsGuest.name = true
+  AccountsGuest.forced = true;
+  AccountsGuest.enable = true;
+  AccountsGuest.anonymous = true;
 });
